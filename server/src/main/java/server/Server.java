a249b7474132d24915460213ec25fbdb794a4c2e
ConnerH1123 <hconner375@gmail.com>
2026-05-19 09:58:28 -0600

    implementing phase3 - server endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,41 @@
 package server;
 
-import io.javalin.*;
+import com.google.gson.Gson;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.Map;
+
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .post("/echo", this::echo);
 
         // Register your endpoints and exception handlers here.
 
+    }
+
+    private void echo(Context context) {
+        // Convert body json to object
+        Map bodyObject = getBodyObject(context, Map.class);
+
+        // Convert bodyObject back to json and send to client
+        String json = new Gson().toJson(bodyObject);
+        context.json(json);
+    }
+
+    private static <T> T getBodyObject(Context context, Class<T> theClass) {
+        var bodyObject = new Gson().fromJson(context.body(), theClass);
+
+        if (bodyObject == null) {
+            throw new RuntimeException("missing required body");
+        }
+
+        return bodyObject;
     }
 
     public int run(int desiredPort) {
