84cf050b070e927c10c8d3a375a9e7a9e06add72
ConnerH1123 <hconner375@gmail.com>
2026-05-19 22:56:56 -0600

    implementing phase3 - server pseudocode

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -13,7 +13,14 @@ public class Server {
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
-                .post("/echo", this::echo);
+                .post("/echo", this::echo)
+                .post("/user",this::register)
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::listGames)
+                .post("/game", this::createGame)
+                .put("/game", this::joinGame)
+                .delete("/db", this::clear);
 
         // Register your endpoints and exception handlers here.
 
@@ -24,8 +31,52 @@ public class Server {
         Map bodyObject = getBodyObject(context, Map.class);
 
         // Convert bodyObject back to json and send to client
-        String json = new Gson().toJson(bodyObject);
+        String json = new Gson().toJson(bodyObject) + "\n";
         context.json(json);
+    }
+
+
+    //Body: {"username": "", "password": "", "email": ""}
+    //Returns: {"username": "", "authToken": ""}
+    private void register(Context ctx) {
+
+    }
+
+    //Body: {"username": "", "password": ""}
+    //Returns: {"username": "", "authToken": ""}
+    private void login(Context ctx) {
+
+    }
+
+    //Header: authToken
+    //Returns: {}
+    private void logout(Context ctx) {
+
+    }
+
+    //Header: authToken
+    //Returns: {"games": [{"gameID": <gameID>, "whiteUsername": "", "blackUsername": "", "gameName": ""}]}
+    private void listGames(Context ctx) {
+
+    }
+
+    //Header: authToken
+    //Body: {"gameName": ""}
+    //Returns: {"games": [{"gameID": <gameID>, "whiteUsername": "", "blackUsername": "", "gameName": ""}]}
+    private void createGame(Context ctx) {
+
+    }
+
+    //Header: authToken
+    //Body: {"playerColor": "", "gameID": <gameID>}
+    //Returns: {}
+    private void joinGame(Context ctx) {
+
+    }
+
+    //Returns: {}
+    private void clear(Context ctx) {
+
     }
 
     private static <T> T getBodyObject(Context context, Class<T> theClass) {
