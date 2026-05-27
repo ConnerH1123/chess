0362b939a69be95d9861a264de8b23b6a058afae
ConnerH1123 <hconner375@gmail.com>
2026-05-27 13:50:43 -0600

    Add debug output to SqlDatabase.configureDatabase()

--- server/src/main/java/dataaccess/SqlDatabase.java
+++ server/src/main/java/dataaccess/SqlDatabase.java
@@ -9,14 +9,25 @@ import static java.sql.Types.NULL;
 
 public class SqlDatabase {
     void configureDatabase(String[] createStatements) throws DataAccessException {
+        System.out.println("DEBUG: configureDatabase() called");
         DatabaseManager.createDatabase();
+        System.out.println("DEBUG: Database created, now getting connection...");
         try (Connection conn = DatabaseManager.getConnection()) {
+            System.out.println("DEBUG: Connection obtained, executing " + createStatements.length + " statements");
             for (String statement : createStatements) {
+                System.out.println("DEBUG: Executing: " + statement.substring(0, Math.min(50, statement.length())) + "...");
                 try (var preparedStatement = conn.prepareStatement(statement)) {
                     preparedStatement.executeUpdate();
+                    System.out.println("DEBUG: Statement executed successfully");
                 }
             }
+            System.out.println("DEBUG: All statements executed successfully");
         } catch (SQLException ex) {
+            System.err.println("DEBUG: SQLException in configureDatabase()");
+            System.err.println("DEBUG: Error Code: " + ex.getErrorCode());
+            System.err.println("DEBUG: SQL State: " + ex.getSQLState());
+            System.err.println("DEBUG: Error Message: " + ex.getMessage());
+            ex.printStackTrace();
             throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
         }
     }
