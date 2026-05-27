3cb4f5e955c6a89e5fa0a30fc7e98d66622289b9
ConnerH1123 <hconner375@gmail.com>
2026-05-26 21:10:52 -0600

    implementing phase4 - get user test

--- server/src/test/java/dataaccess/MySqlUserDAOTest.java
+++ server/src/test/java/dataaccess/MySqlUserDAOTest.java
@@ -1,17 +1,26 @@
 package dataaccess;
 
 import model.UserData;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class MySqlUserDAOTest {
     private static DatabaseManager db;
+    private UserDAO userDAO;
 
+    @AfterEach
+    public void tearDown() {
+        try {
+            userDAO.deleteAllUsers();
+        } catch (Exception e) {
+            //
+        }
+
+    }
 
     @Test
     public void testCreateUser() {
         try {
-            UserDAO userDAO = new MySqlUserDAO();
+            userDAO = new MySqlUserDAO();
             UserData userData = new UserData("joe", "joe mama", "xD");
             userDAO.createUser(userData);
         } catch (Exception e) {
@@ -20,16 +29,32 @@ public class MySqlUserDAOTest {
     }
 
     @Test
+    public void testGetUser() {
+        try {
+            userDAO = new MySqlUserDAO();
+            String username = "joe";
+            String password = "joe mama";
+            String email = "xD";
+            UserData expectedData = new UserData(username, password, email);
+            userDAO.createUser(expectedData);
+            UserData actualData = userDAO.getUser(username);
+            Assertions.assertEquals(expectedData.username(), actualData.username());
+            Assertions.assertEquals(expectedData.email(), actualData.email());
+        } catch (Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
     public void testDeleteAllUsers() {
         try {
-            UserDAO userDAO = new MySqlUserDAO();
+            userDAO = new MySqlUserDAO();
             UserData userData = new UserData("Bob", "password", "email");
             userDAO.createUser(userData);
             userData = new UserData("Sue", "password", "email");
             userDAO.createUser(userData);
             userData = new UserData("Henry", "password", "email");
             userDAO.createUser(userData);
-            userDAO.deleteAllUsers();
         } catch (Exception e) {
             Assertions.fail(e);
         }
