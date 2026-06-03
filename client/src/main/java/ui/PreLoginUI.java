d00aa3c8c5fbeec0e0069c368109c867019e82ef
ConnerH1123 <hconner375@gmail.com>
2026-06-03 12:06:53 -0600

    implementing phase5 - pre-login UI register

--- client/src/main/java/ui/PreLoginUI.java
+++ client/src/main/java/ui/PreLoginUI.java
@@ -1,6 +1,8 @@
 package ui;
 
+import client.ResponseException;
 import client.ServerFacade;
+import request.RegisterRequest;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -20,6 +22,7 @@ public class PreLoginUI extends UserInterface {
             System.out.print("[LOGGED_OUT] >>> ");
             String line = scanner.nextLine();
             result = eval(line);
+            System.out.println(result);
         }
     }
 
@@ -27,22 +30,41 @@ public class PreLoginUI extends UserInterface {
         String[] tokens = input.toLowerCase().split(" ");
         String cmd = (tokens.length > 0) ? tokens[0] : "help";
         String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
-        return switch (cmd) {
-            case "quit" -> "quit";
-            case "help" -> help();
-            default -> {
-                System.out.print(cmd + " was not a recognized command. ");
-                yield help();
-            }
-        };
+        try {
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "quit" -> "quit";
+                case "help" -> help();
+                default -> {
+                    System.out.print("'" + cmd + "' was not a recognized command. ");
+                    yield help();
+                }
+            };
+        } catch (ResponseException e) {
+            return e.getMessage();
+        }
+
+    }
+
+    private String register(String... params) throws ResponseException {
+        if (params.length >= 3) {
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+            RegisterRequest request = new RegisterRequest(username, password, email);
+            server.register(request);
+            return String.format("User %s successfully registered", username);
+        }
+        throw new ResponseException("Insufficient arguments. Expected: <USERNAME> <PASSWORD> <EMAIL>");
     }
 
     private String help() {
-        System.out.println("These are your options:");
-        System.out.println("  register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
-        System.out.println("  login <USERNAME> <PASSWORD> - to play chess");
-        System.out.println("  quit - playing chess");
-        System.out.println("  help - with possible commands");
-        return "help";
+        return """
+                These are your options:
+                  register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                  login <USERNAME> <PASSWORD> - to play chess
+                  quit - playing chess
+                  help - with possible commands
+                """;
     }
 }
